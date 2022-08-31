a187184df2160bf71a7a063d40bf3801f659659b
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-08-31 16:52:12 +1200

    pagination is implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Store from './pages/Store';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
   );
 }
