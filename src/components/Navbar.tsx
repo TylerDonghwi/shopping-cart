1d4dfdaf7959ebc79b8e7e51f1704f16a4046f58
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-09-01 18:27:12 +1200

    shopping cart displays how many items there are in the cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,13 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { useShoppingCart } from '../context/ShoppingCartContext';
 
 export default function Navbar() {
 
     const [path, setPath] = useState('/')
+
+    const { openCart, cartQuantity } = useShoppingCart()
 
     return (
         <div className="sticky bg-white shadow-md flex flex-row items-center h-[4.5rem]">
@@ -23,11 +26,18 @@ export default function Navbar() {
                     {path === '/about' ? <hr className='relative left-[15px] top-[1.5rem] border-blue-500 w-[50px]'></hr> : null}
                 </Link>
             </>
+            {cartQuantity > 0 && (
+                <button
+                    className='relative ml-auto h-12 right-[0.625rem] w-12 bg-white border rounded flex justify-center items-center hover:bg-slate-50 active:bg-slate-100 transition-all duration-200'
+                    onClick={openCart}
+                >
+                    <FontAwesomeIcon className='text-blue-500 h-[18px]' icon={faCartShopping}></FontAwesomeIcon>
+                    <div className='absolute rounded-full bg-red-500 flex justify-center items-center text-white h-5 w-5 right-[-5px] bottom-[-5px] text-[14px]'>
+                        {cartQuantity}
+                    </div>
+                </button>
+            )}
 
-            <button className='relative ml-auto h-12 right-[0.625rem] w-12 bg-white border rounded flex justify-center items-center hover:bg-slate-50 active:bg-slate-100 transition-all duration-200'>
-                <FontAwesomeIcon className='text-blue-500 h-[18px]' icon={faCartShopping}></FontAwesomeIcon>
-                <div className='absolute rounded-full bg-red-500 flex justify-center items-center text-white h-5 w-5 right-[-5px] bottom-[-5px] text-[14px]'>3</div>
-            </button>
         </div>
     )
 }
