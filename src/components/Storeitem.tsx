64a2820b3e4ba6aa8eec8a0f2ae3f66cdc03bf27
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-09-01 16:55:26 +1200

    display for whether if there is the corresponding item in the cart is implemented

--- src/components/Storeitem.tsx
+++ src/components/Storeitem.tsx
@@ -8,14 +8,38 @@ type StoreItemProps = {
 }
 
 export default function Storeitem({ id, name, price, imgUrl }: StoreItemProps) {
+    const quantity = Math.random()
     return (
-        <div className='m-2 shadow-md rounded-md'>
+        <div className='m-2 border shadow-md rounded-md'>
             <img alt={name} src={imgUrl} className='rounded-tl-md rounded-tr-md h-[200px] w-[100%] object-cover' />
             <div className='flex flex-col'>
-                <div className='p-2 flex flex-row justify-between align-baseline mb-4'>
+                <div className='p-2 flex flex-row justify-between align-baseline'>
                     <span className='text-lg flex flex-wrap'>{name}</span>
-                    <span className='ml-2 text-slate-500'>{formatCurrency(price)}</span>
+                    <span className='text-slate-500'>{formatCurrency(price)}</span>
                 </div>
+            </div>
+            <div className='mt-auto w-[100%]'>
+                {quantity === 0 ? (
+                    <button className='relative text-white rounded-sm p-1 bg-blue-500 w-[calc(100%-1rem)] m-2 hover:bg-blue-600 active:bg-blue-700 transition-all duraction-100'>
+                        + Add To Cart
+                    </button>
+                ) : (
+                    <div className='flex flex-col justify-center items-center m-2'>
+                        <div className='flex flex-row justify-center items-center w-[100%] mb-2 [&>*]:mx-2 [&>button]:w-8'>
+                            <button className='relative text-white rounded-sm p-1 bg-blue-500 hover:bg-blue-600 active:bg-blue-700'>
+                                -
+                            </button>
+                            <div>
+                                <span className='text-lg'>{Math.ceil(quantity)}</span> in cart
+                            </div>
+                            <button className='relative text-white rounded-sm p-1 bg-blue-500 hover:bg-blue-600 active:bg-blue-700'>
+                                +
+                            </button>
+                        </div>
+                        <button className='relative text-white rounded-sm p-1 bg-red-500 w-[25%]  hover:bg-red-600 active:bg-red-700 transition-all duraction-100'>
+                            Remove
+                        </button>
+                    </div>)}
             </div>
         </div>
     )
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -4,11 +4,10 @@ import Storeitem from '../components/Storeitem'
 export default function Store() {
     return (
         <>
-            <h1>Store</h1>
+            <h1 className='text-[24px] font-bold'>Store</h1>
             <div className='grid sm:grid-cols-2 xs:grid-cols-1 lg:grid-cols-3'>
                 {storeItems.map(item => (
-
-                    <div><Storeitem {...item} /></div>
+                    <div key={item.id}><Storeitem {...item} /></div>
                 ))}
 
             </div>
